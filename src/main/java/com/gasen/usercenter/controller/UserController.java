74971b1a2b6d3d2c44d13f3cfa4c7cddfc638e8b
GASEN <GASEN1216@163.com>
2023-12-30 20:55:57 +0800

    test

--- src/main/java/com/gasen/usercenter/controller/UserController.java
+++ src/main/java/com/gasen/usercenter/controller/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
